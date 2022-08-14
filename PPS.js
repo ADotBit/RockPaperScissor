c72c306f583f7f3a4547889ed110a9cd0c4524a6
Axel <eldrank@gmail.com>
2022-08-14 20:29:29 +0200

    Add function game, add rounds, add scores

--- PPS.js
+++ PPS.js
@@ -1,49 +1,49 @@
-
 let choice = ["Pierre", "Feuille", "Ciseaux"];
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice(){
     let randomChoice = Math.floor(Math.random()*choice.length);
     return randomChoice;
 }
 
-function getPlayerChoice(){
-    let playerChoice = prompt("Choississez P pour Pierre, F pour Feuille ou C pour Ciseaux: ");
-    return playerChoice;
-}
-
 function playRound(playerSelection, computerSelection){
-    if (playerSelection.toUpperCase() === "P"){
-        if (computerSelection === "Pierre"){
-            alert("Egalité !");
-        }else if (computerSelection === "F"){
-            alert("Perdu ! La feuille bat la pierre.");
-        }else{
-            alert("Gagné ! La pierre bat les ciseaux.");
-        }
-    }
-
-    if (playerSelection.toUpperCase() === "F"){
-        if (computerSelection === "Pierre"){
-            alert("Gagné ! La feuille bat la Pierre");
-        }else if (computerSelection === "Feuille"){
-            alert("Egalité !");
-        }else{
-            alert("Perdu ! Les ciseaux coupent la feuille.");
-        }
-    }
-
-    if (playerSelection.toUpperCase() === "Ciseaux"){
-        if (computerSelection === "Pierre"){
-            alert("Perdu ! La pierre bat les ciseaux.");
-        }else if (computerSelection === "Feuille"){
-            alert("Gagné ! Les ciseaux coupent la pierre.");
-        }else{
-            alert("Egalité !");
-        }
+    if (playerSelection === "P" && computerSelection === "Ciseaux"){
+        playerScore++;
+        return "T'as gagné ! La pierre écrase les ciseaux.";
+    }else if (playerSelection === "F" && computer === "Pierre"){
+        playerScore++;
+        return "T'as gagné ! La feuille couvre la pierre.";
+    }else if (playerSelection === "C" && computerSelection === "Feuille"){
+        playerScore++;
+        return "T'as gagné ! Les ciseaux coupent la feuille.";
+    }else if (playerSelection === "P" && computerSelection === "Feuille"){
+        computerScore++;
+        return "Perdu ! La feuille couvre la pierre.";
+    }else if (playerSelection === "F" && computerSelection === "Ciseaux"){
+        computerScore++;
+        return "Perdu ! Les ciseaux coupent la feuille.";
+    }else if (playerSelection === "C" && computerSelection === "Pierre"){
+        computerScore++;
+        return "Perdu ! La pierre écrase les ciseaux."
+    }else{
+        return "It's a tie."
     }
 }
 
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice()
+function game(){
+    for (let round = 0; round < 5; round++){
+        const playerSelection = prompt("Choississez P pour Pierre, F pour Feuille ou C pour Ciseaux: ").toUpperCase;
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+    }
 
-console.log(playRound(playerSelection, computerSelection));
+    if (playerScore > computerScore){
+        return "Vous avez gagné la partie.";
+    }else if (computerScore > playerScore){
+        return "Vous avez perdu la partie.";
+    }
+
+}
+
+game()
