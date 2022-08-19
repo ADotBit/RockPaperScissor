71785aa946b6be32623aa069d4199acc9d14eb1c
Axel <eldrank@gmail.com>
2022-08-19 18:22:55 +0200

    make buttons simpler, make checkWinner, make update score

--- PPS.js
+++ PPS.js
@@ -1,72 +1,68 @@
 let playerScore = 0;
 let computerScore = 0;
+const logD = document.querySelector(".log");
+const playerRun = document.querySelector(".player");
+const computerRun = document.querySelector(".computer");
+const buttons = document.querySelectorAll("button");
 
-const pBtn = document.querySelector(".pierre");
-const fBtn = document.querySelector(".feuille");
-const cBtn = document.querySelector(".ciseaux");
+//Make button
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const computerSelection = getComputerChoice();
+        const playerSelection = `${button.className}`; 
+        playRound(computerSelection, playerSelection);
+        updateScore(playerScore, computerScore);
+        checkWinner(playerScore, computerScore);
+    })
+})
 
-
-
+// Make computer choice  in array
 function getComputerChoice(){
     let choice = ["pierre", "feuille", "ciseaux"];
     let randomChoice = Math.floor(Math.random()*choice.length);
-    return choice[randomChoice];
+    return choice[randomChoice]; //I forgot the brackets to precise it is an array
 }
 
 function playRound(playerSelection, computerSelection){
-    const logD = document.querySelector(".log");
-
+    const txt = document.createElement("p");
     if (playerSelection === "pierre" && computerSelection === "ciseaux"){
         playerScore++;
-        const txt = document.createElement("p");
         txt.textContent = "T'as gagné ! La pierre écrase les ciseaux.";
-        logD.appendChild(txt);
     }else if (playerSelection === "feuille" && computerSelection === "pierre"){
         playerScore++;
-        const txt = document.createElement("p");
         txt.textContent = "T'as gagné ! La feuille couvre la pierre.";
-        logD.appendChild(txt);
     }else if (playerSelection === "ciseaux" && computerSelection === "feuille"){
         playerScore++;
-        const txt = document.createElement("p");
         txt.textContent = "T'as gagné ! Les ciseaux coupent la feuille.";
-        logD.appendChild(txt);
     }else if (playerSelection === "pierre" && computerSelection === "feuille"){
         computerScore++;
-        const txt = document.createElement("p");
         txt.textContent = "Perdu ! La feuille couvre la pierre.";
-        logD.appendChild(txt);
     }else if (playerSelection === "feuille" && computerSelection === "ciseaux"){
         computerScore++;
-        const txt = document.createElement("p");
         txt.textContent = "Perdu ! Les ciseaux coupent la feuille.";
-        logD.appendChild(txt);
     }else if (playerSelection === "ciseaux" && computerSelection === "pierre"){
         computerScore++;
-        const txt = document.createElement("p");
         txt.textContent = "Perdu ! La pierre écrase les ciseaux."
-        logD.appendChild(txt);
     }else if (playerSelection === computerSelection){
-        const txt = document.createElement("p");
-        txt.textContent = `Egalité ! Vous avez tous les deux choisis ${playerSelection}`;
-        logD.appendChild(txt);
+        txt.textContent = `Egalité ! Vous avez tous les deux choisis ${playerSelection}.`;   
     }
+    logD.appendChild(txt);
 }
 
-pBtn.addEventListener("click", () => {
-    const computerSelection = getComputerChoice();
-    const playerSelection = "pierre";
-    playRound(computerSelection, playerSelection)
-})
+function updateScore(playerScore, computerScore){
+    playerRun.textContent = `${playerScore}`;
+    computerRun.textContent = `${computerScore}`
+}
 
-fBtn.addEventListener("click", () => {
-    const computerSelection = getComputerChoice();
-    const playerSelection = "feuille";
-    playRound(computerSelection, playerSelection)
-})
+function checkWinner(playerScore, computerScore) {
+    const h2 = document.createElement("h2")
+    if (playerScore === 5){
+        h2.textContent = `Vous avez gagné ${playerScore} points contre ${computerScore} points.`
+        logD.append(h2);
+    }else if (computerScore === 5){
+        h2.textContent = `Vous avez perdu ${playerScore} points contre ${computerScore} points.`
+        logD.append(h2);
+    }
 
-cBtn.addEventListener("click", () => {
-    const computerSelection = getComputerChoice();
-    const playerSelection = "ciseaux";
-    playRound(computerSelection, playerSelection)
-})
+}
+
